9e6af4dce9a58c36581caa846ee80c11c1120fa2
Devansh-ops <72489302+Devansh-ops@users.noreply.github.com>
2021-01-01 18:30:46 +0530

    Update zipcracker.py

--- zipcracker.py
+++ zipcracker.py
@@ -2,16 +2,24 @@ import argparse
 import os
 import sys
 ###############
-parser = argparse.ArgumentParser(prog="zipcracker",description="Crack password protected zip file")
+def main():
+    parser = argparse.ArgumentParser(prog="zipcracker",description="Crack password protected zip file")
 
-parser.add_argument('Path', metavar='path', type=str,help="The path to the list")
+    parser.add_argument('-i',metavar="input_file", type=str, help="The path to the zip file",required=True)
+    parser.add_argument('-t',metavar="type",type=str,help="The type of attack", default='d')
+    parser.add_argument('-d',metavar="dict_file", type=str, help="The path to dictionary file")
+    args = parser.parse_args()
 
-args = parser.parse_args()
+    print(args)
 
-input_path = args.Path
+    input_path = args.i
 ###############
+'''
 if not os.path.isdir(input_path):
     print('The path specified does not exist')
     sys.exit()
 
 print('\n'.join(os.listdir(input_path)))
+'''
+if __name__ == '__main__':
+    main()
