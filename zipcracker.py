3957f42703e6e71c7b94bc8865ba776e576d7b38
Devansh-ops <72489302+Devansh-ops@users.noreply.github.com>
2021-01-02 04:50:00 +0530

    works

--- zipcracker.py
+++ zipcracker.py
@@ -1,37 +1,48 @@
 import argparse
+<<<<<<< Updated upstream
 import os
 import sys
+=======
+>>>>>>> Stashed changes
 import zipfile
+#from tqdm import tqdm
 from multiprocessing import Process
 
 def extractFile(zfile, password):
-    try:
-        print("trying",password)
-        zfile.extractall(pwd=password)
-        print('[+] Found Password:',password)
-        exit(0)
-    except:
-        pass
-
+    with zipfile.ZipFile(zfile) as zFile:
+        try:
+            password_encoded = bytes(password.encode('utf-8'))
+            zFile.setpassword(password_encoded)
+            zFile.testzip()
+            print ("[+] Password Found: " + password + '\n')
+            exit(0)
+        except RuntimeError:
+            pass
 def main():
-    parser = argparse.ArgumentParser(prog="zipcracker",description="Crack password protected zip file")
+    parser = argparse.ArgumentParser(prog="zipcracker",description="Crack password protected zip file using dictionary attack")
 
     parser.add_argument('-i',metavar="input_file", type=str, help="The path to the zip file",required=True)
-    parser.add_argument('-t',metavar="type",type=str,help="The type of attack", default='d')
-    parser.add_argument('-d',metavar="dict_file", type=str, help="The path to dictionary file")
+    parser.add_argument('-d',metavar="dict_file", type=str, help="The path to dictionary file",required=True)
     args = parser.parse_args()
 
     try:
         #zfile = zipfile.ZipFile(args.i)
         zfile = args.i
         dfile = open(args.d)
-        process = []
-
+        #process = []
+        #n_words = len(list(dfile))
+        #print("Total passwords to test:", n_words)
         for line in dfile.readlines():
             pw = line.strip("\n")
+            print(pw)
+            '''
             p = Process(target=extractFile, args=(zfile,pw))
             p.start()
             p.join()
+            '''
+            extractFile(zfile,pw)
+    except:
+        pass
     finally:
         '''
         zfile.close()
